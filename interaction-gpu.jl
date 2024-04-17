907bafe0f1104d72be24362442d6ac729c2f6d85
flowlab <rymanderson@gmail.com>
2024-04-17 01:19:48 -0600

    Continue adding array operations

--- interaction-gpu.jl
+++ interaction-gpu.jl
@@ -3,9 +3,11 @@ using BenchmarkTools
 using Random
 using Statistics
 
+const const4 = 1/(4*pi)
 
 @inline g_val(r) = r^3 * (r^2 + 2.5) / (r^2 + 1)^2.5
 @inline dg_val(r) = 7.5 * r^2 / ((r^2 + 1)^2.5*(r^2 + 1))
+@inline cross_op(v) = [0.0 -v[3] v[2]; v[3] 0.0 -v[1]; -v[2] v[1] 0.0];
 
 function get_inputs(nparticles, nfields; seed=1234, T=Float32)
     Random.seed!(seed)
@@ -28,43 +30,106 @@ function cpu_interact(s, t)
             # Operation 2
             r2 = dX1*dX1 + dX2*dX2 + dX3*dX3
             r = sqrt(r2)
+            r3 = r * r2
+
+            g_sgm = g_val(r/ps[7])
+            dg_sgmdr = dg_val(r/ps[7])
+
+            crss1 = -const4 / r^3 * ( dX2*ps[6] - dX3*ps[5] )
+            crss2 = -const4 / r^3 * ( dX3*ps[4] - dX1*ps[6] )
+            crss3 = -const4 / r^3 * ( dX1*ps[5] - dX2*ps[4] )
+
+            pt[10] += g_sgm * crss1
+            pt[11] += g_sgm * crss2
+            pt[12] += g_sgm * crss3
+
+            aux = dg_sgmdr/(ps[7]*r) - 3*g_sgm /r^2
+
+            pt[16] += aux * crss1 * dX1
+            pt[17] += aux * crss2 * dX2
+            pt[18] += aux * crss3 * dX3
+
+            pt[19] += aux * crss1 * dX2
+            pt[20] += aux * crss2 * dX2
+            pt[21] += aux * crss3 * dX2
+
+            pt[22] += aux * crss1 * dX3
+            pt[23] += aux * crss2 * dX3
+            pt[24] += aux * crss3 * dX3
+
+            aux = -const4 * g_sgm/r3
         end
     end
 end
 
 function gpu_interact(s, t)
-    # Allocate main GPU arrays
-    sx_d = CuArray(view(s, 1:3, :))
-    # t_d = CuArray(view(t, :, :))
-    tx_d = CuArray(view(t, 1:3, :))
-
     # Allocate required intermmediate arrays
     nt = size(t, 2)
+    ns = size(s, 2)
+
     dX = CuArray{eltype(t)}(undef, (3, nt))
-    r2 = CuArray{eltype(t)}(undef, (1, nt))
+    crss = similar(dX)
+    U = CUDA.zeros(size(dX))
+    J1 = CUDA.zeros(size(dX))
+    J2 = CUDA.zeros(size(dX))
+    J3 = CUDA.zeros(size(dX))
+
     r = CuArray{eltype(t)}(undef, (1, nt))
+    r2 = similar(r)
+    r3 = similar(r)
+    rbysigma = similar(r)
+    g_sgm = similar(r)
+    dg_sgm = similar(r)
 
     dX_cpu = Array{eltype(t)}(undef, (3, nt))
 
+    s_sigma = view(s, 7, :)
+    s_gamma_mat = CuArray{eltype(t)}(undef, (3, 3))
+
+    # Loop over each source
     for i = 1:size(s, 2)
-        # Operation 1
         dX_cpu .= view(t, 1:3, :) .- view(s, 1:3, i)
         copyto!(dX, dX_cpu)
-        # Operation 2
         r2 .= mapreduce(x->x^2, +, dX, dims=1)
         # r2 .= CUDA.sum(CUDA.abs2, dX, dims=1)
         r .= CUDA.sqrt.(r2)
+        r3 = r .* r2
+
+        rbysigma .= r / s_sigma[i]
+        g_sgm .= map(g_val, rbysigma)
+        dg_sgm .= map(dg_val, rbysigma)
+
+        copyto!(s_gamma_mat, cross_op(view(s, 4:6, i)))
+        crss .= const4 * (s_gamma_mat * dX) ./ r3
+
+        U .+= g_sgm .* crss
+
+        aux = dg_sgm ./ (r * s_sigma[i]) .- 3*map(/, g_sgm, r2)
+        dX .= aux .* dX
+
+        J1 .+= reshape(dX[1, :], size(r)) .* crss
+        J2 .+= reshape(dX[2, :], size(r)) .* crss
+        J3 .+= reshape(dX[3, :], size(r)) .* crss
+
+        # aux = -const4 * map(/, g_sgm, r3)
+        # Jterm1 = aux * view(s, 4, i)
+
     end
+        # t[10:12, :] .= Array(U)
+        copyto!(t[10:12, :], U)
+        copyto!(t[16:18, :], J1)
+        copyto!(t[19:21, :], J2)
+        copyto!(t[22:24, :], J3)
 end
 
 # ns = 2 .^ collect(4:2:20)
-n = 2^12
+n = 2^20
 nfields = 43
 
 # for n in ns
-    src, trg, src2, trg2 = get_inputs(n, nfields)
-    t_cpu = @benchmark cpu_interact($src, $trg)
-    t_gpu = @benchmark CUDA.@sync gpu_interact($src, $trg)
-    speedup = mean(t_cpu.times)/mean(t_gpu.times)
-    println(n, " ", speedup)
+src, trg, src2, trg2 = get_inputs(n, nfields)
+t_cpu = @benchmark cpu_interact($src, $trg)
+t_gpu = @benchmark CUDA.@sync gpu_interact($src, $trg)
+speedup = mean(t_cpu.times)/mean(t_gpu.times)
+println(n, " ", speedup)
 # end
