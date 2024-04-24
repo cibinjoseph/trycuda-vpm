b860d587e2c9c9e06c2d69efad310cf9206faa58
flowlab <rymanderson@gmail.com>
2024-04-24 14:39:55 -0600

    Remove device keyword to avoid highlighting

--- nvidia/nbody1.c
+++ nvidia/nbody1.c
@@ -1,4 +1,4 @@
-__device__ float3 bodyBodyInteraction(float4 bi, float4 bj, float3 ai) {
+bodyBodyInteraction(float4 bi, float4 bj, float3 ai) {
   float3 r;   // r_ij  [3 FLOPS]
   r.x = bj.x - bi.x;   r.y = bj.y - bi.y;   r.z = bj.z - bi.z;   // distSqr = dot(r_ij, r_ij) + EPS^2  [6 FLOPS]
   float distSqr = r.x * r.x + r.y * r.y + r.z * r.z + EPS2;   // invDistCube =1/distSqr^(3/2)  [4 FLOPS (2 mul, 1 sqrt, 1 inv)]
