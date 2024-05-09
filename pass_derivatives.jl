3d0a47dd614bd93b79bda827b10f2e89ca110ded
flowlab <rymanderson@gmail.com>
2024-05-09 17:32:23 -0600

    Get ForwardAD duals passing through the GPU for simple problem

--- pass_derivatives.jl
+++ pass_derivatives.jl
@@ -1,5 +1,7 @@
 using FiniteDiff
 using ForwardDiff
+using CUDA
+using Random
 
 include("gravitation.jl")
 
@@ -35,8 +37,46 @@ function get_net_interaction(x::Vector{T}) where T
     return vel
 end
 
+function cpu_func!(y, x)
+    @show x
+    for i = 1:length(x)
+        y[i] = x[i]*x[i] + 2
+    end
+    return
+end
+
+function gpu_func!(y, x)
+    idx::Int32 = threadIdx().x + (blockIdx().x-1)*blockDim().x
+
+    @cushow typeof(x)
+
+    if idx <= length(x)
+        y[idx] = x[idx]*x[idx] + 2.0f0
+    end
+    return
+end
+
+function gpu_runner!(y::Vector{T}, x::Vector{T}) where T
+    x_d = CuArray(view(x, :, :))
+    y_d = similar(x_d)
+    n = length(x)
+    nthreads = min(n, 2^5)
+    nblocks = ceil(Int32, n/nthreads)
+    CUDA.@sync @cuda threads=nthreads blocks=nblocks gpu_func!(y_d, x_d)
+    y .= Array(y_d)
+    return
+end
+
 r = 5.0
 gamma = 2.0
-df_ad = ForwardDiff.gradient(get_net_interaction, [r, gamma])
-df_fd = FiniteDiff.finite_difference_gradient(get_net_interaction, [r, gamma])
-@assert isapprox(df_ad, df_fd; atol=tol)
+# df_ad = ForwardDiff.gradient(get_net_interaction, [r, gamma])
+# df_fd = FiniteDiff.finite_difference_gradient(get_net_interaction, [r, gamma])
+# @assert isapprox(df_ad, df_fd; atol=tol)
+
+n = 2^2
+Random.seed!(1234)
+x = rand(Float32, n)
+y = similar(x)
+df_cpu = ForwardDiff.jacobian(cpu_func!, y, x)
+df_gpu = ForwardDiff.jacobian(gpu_runner!, y, x)
+@assert isapprox(df_cpu, df_gpu; atol=tol)
