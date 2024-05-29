57c424bcaff7441c1a92504409693dad464209fa
flowlab <rymanderson@gmail.com>
2024-05-29 14:39:18 -0600

    Finish VPM kernel

--- vpm.jl
+++ vpm.jl
@@ -2,6 +2,7 @@ using CUDA
 using BenchmarkTools
 using Random
 using Statistics
+using StaticArrays
 
 const eps2 = 1e-6
 const const4 = 0.25/pi
@@ -28,10 +29,10 @@ end
     r3 = r*r2
 
     # Mapping to variables
-    @inbounds sigma = s[7, j]
     @inbounds gam1 = s[4, j]
     @inbounds gam2 = s[5, j]
     @inbounds gam3 = s[6, j]
+    @inbounds sigma = s[7, j]
 
     # Regularizing function and deriv
     g_sgm = g_val(r/sigma)
@@ -87,10 +88,10 @@ end
     r3 = r*r2
 
     # Mapping to variables
-    @inbounds sigma = s[7, j]
     @inbounds gam1 = s[4, j]
     @inbounds gam2 = s[5, j]
     @inbounds gam3 = s[6, j]
+    @inbounds sigma = s[7, j]
 
     # Regularizing function and deriv
     g_sgm = g_val(r/sigma)
@@ -102,39 +103,41 @@ end
     @inbounds crss3 = -const4 / r3 * ( dX1*gam2 - dX2*gam1 )
 
     # U = ∑g_σ(x-xp) * K(x-xp) × Γp
-    @inbounds t[10, i] += g_sgm * crss1
-    @inbounds t[11, i] += g_sgm * crss2
-    @inbounds t[12, i] += g_sgm * crss3
+    @inbounds u1 = g_sgm * crss1
+    @inbounds u2 = g_sgm * crss2
+    @inbounds u3 = g_sgm * crss3
 
     # ∂u∂xj(x) = ∑[ ∂gσ∂xj(x−xp) * K(x−xp)×Γp + gσ(x−xp) * ∂K∂xj(x−xp)×Γp ]
     # ∂u∂xj(x) = ∑p[(Δxj∂gσ∂r/(σr) − 3Δxjgσ/r^2) K(Δx)×Γp
     aux = dg_sgmdr/(sigma*r) - 3*g_sgm /r2
     # j=1
-    @inbounds t[16, i] += aux * crss1 * dX1
-    @inbounds t[17, i] += aux * crss2 * dX1
-    @inbounds t[18, i] += aux * crss3 * dX1
+    @inbounds j1 = aux * crss1 * dX1
+    @inbounds j2 = aux * crss2 * dX1
+    @inbounds j3 = aux * crss3 * dX1
     # j=2
-    @inbounds t[19, i] += aux * crss1 * dX2
-    @inbounds t[20, i] += aux * crss2 * dX2
-    @inbounds t[21, i] += aux * crss3 * dX2
+    @inbounds j4 = aux * crss1 * dX2
+    @inbounds j5 = aux * crss2 * dX2
+    @inbounds j6 = aux * crss3 * dX2
     # j=3
-    @inbounds t[22, i] += aux * crss1 * dX3
-    @inbounds t[23, i] += aux * crss2 * dX3
-    @inbounds t[24, i] += aux * crss3 * dX3
+    @inbounds j7 = aux * crss1 * dX3
+    @inbounds j8 = aux * crss2 * dX3
+    @inbounds j9 = aux * crss3 * dX3
 
     # ∂u∂xj(x) = −∑gσ/(4πr^3) δij×Γp
     # Adds the Kronecker delta term
     aux = -const4 * g_sgm / r3
 
     # j=1
-    @inbounds t[17, i] -= aux * gam3
-    @inbounds t[18, i] += aux * gam2
+    @inbounds j2 -= aux * gam3
+    @inbounds j3 += aux * gam2
     # j=2
-    @inbounds t[19, i] += aux * gam3
-    @inbounds t[21, i] -= aux * gam1
+    @inbounds j4 += aux * gam3
+    @inbounds j6 -= aux * gam1
     # j=3
-    @inbounds t[22, i] -= aux * gam2
-    @inbounds t[23, i] += aux * gam1
+    @inbounds j7 -= aux * gam2
+    @inbounds j8 += aux * gam1
+
+    return u1, u2, u3, j1, j2, j3, j4, j5, j6, j7, j8, j9
 end
 
 function cpu_gravity!(s, t)
@@ -185,21 +188,19 @@ function gpu_gravity3!(s, t, num_cols)
     n_tiles::Int32 = t_size/tile_dim
     bodies_per_col::Int32 = tile_dim / num_cols
 
-    sh_mem = CuDynamicSharedArray(eltype(t), (4, tile_dim))
+    sh_mem = CuDynamicSharedArray(eltype(t), (7, tile_dim))
 
-    acc1 = zero(eltype(s))
-    acc2 = zero(eltype(s))
-    acc3 = zero(eltype(s))
+    U = @MVector zeros(eltype(t), 3)
+    J = @MVector zeros(eltype(t), 9)
 
     itile::Int32 = 1
     while itile <= n_tiles
         # Each thread will copy source coordinates corresponding to its index into shared memory. This will be done for each tile.
         if (col == 1)
             idx::Int32 = row + (itile-1)*tile_dim
-            @inbounds sh_mem[1, row] = s[1, idx]
-            @inbounds sh_mem[2, row] = s[2, idx]
-            @inbounds sh_mem[3, row] = s[3, idx]
-            @inbounds sh_mem[4, row] = s[4, idx]
+            for idim::Int32 = 1:7
+                @inbounds sh_mem[idim, row] = s[idim, idx]
+            end
         end
         sync_threads()
 
@@ -210,9 +211,12 @@ function gpu_gravity3!(s, t, num_cols)
             out = gpu_interaction!(tx, ty, tz, sh_mem, i_source)
 
             # Sum up accelerations for each source in a tile
-            @inbounds acc1 += out[1]
-            @inbounds acc2 += out[2]
-            @inbounds acc3 += out[3]
+            for idim::Int32 = 1:3
+                @inbounds U[idim] += out[idim]
+            end
+            for idim::Int32 = 1:9
+                @inbounds J[idim] += out[idim+3]
+            end
             i += 1
         end
         itile += 1
@@ -220,9 +224,12 @@ function gpu_gravity3!(s, t, num_cols)
     end
 
     # Sum up accelerations for each target/thread
-    @inbounds CUDA.@atomic t[5, itarget] += acc1
-    @inbounds CUDA.@atomic t[6, itarget] += acc2
-    @inbounds CUDA.@atomic t[7, itarget] += acc3
+    for idx = 1:3
+        @inbounds CUDA.@atomic t[9+idx, itarget] += U[idx]
+    end
+    for idx = 1:9
+        @inbounds CUDA.@atomic t[15+idx, itarget] += J[idx]
+    end
     return
 end
 
@@ -255,11 +262,12 @@ function benchmark2_gpu!(s, t, p)
         @cuda threads=threads blocks=blocks shmem=shmem gpu_gravity2!(s_d, t_d)
     end
 
-    view(t, 5:7, :) .= Array(t_d[end-2:end, :])
+    view(t, 10:12, :) .= Array(t_d[10:12, :])
+    view(t, 16:24, :) .= Array(t_d[16:24, :])
 end
 
 function benchmark3_gpu!(s, t, p, q)
-    s_d = CuArray(view(s, 1:4, :))
+    s_d = CuArray(view(s, 1:7, :))
     t_d = CuArray(t)
 
     # Num of threads in a tile should always be 
@@ -267,12 +275,13 @@ function benchmark3_gpu!(s, t, p, q)
     # or limited by memory size
     threads::Int32 = p*q
     blocks::Int32 = cld(size(s, 2), p)
-    shmem = sizeof(eltype(t)) * 4 * p
+    shmem = sizeof(eltype(t)) * 7 * p  # XYZ + UVW + J = 7 variables
     CUDA.@sync begin
         @cuda threads=threads blocks=blocks shmem=shmem gpu_gravity3!(s_d, t_d, q)
     end
 
-    view(t, 5:7, :) .= Array(t_d[end-2:end, :])
+    view(t, 10:12, :) .= Array(t_d[10:12, :])
+    view(t, 16:24, :) .= Array(t_d[16:24, :])
 end
 
 function check_launch(n, p, q)
@@ -285,14 +294,12 @@ function check_launch(n, p, q)
     @assert p%q == 0
 end
 
-function main(run_option; nparticles=2^5, p=1, q=1, T=Float32)
+function main(run_option; nparticles=2^5, p=1, q=1, T=Float32, debug=false)
     nfields = 43
     if run_option == 1 || run_option == 2
         println("No. of particles: $nparticles")
         # No. of threads in a block
-        p = min(p, nparticles)
         # No. of columns in a block
-        q = 1
         println("Tile size, p: $p")
         println("Cols per tile, q: $q")
 
@@ -301,18 +308,18 @@ function main(run_option; nparticles=2^5, p=1, q=1, T=Float32)
         src, trg, src2, trg2 = get_inputs(nparticles, nfields; T=T)
         if run_option == 1
             cpu_gravity!(src, trg)
-            benchmark1_gpu!(src2, trg2)
-            # benchmark3_gpu!(src2, trg2, p, q)
+            # benchmark1_gpu!(src2, trg2)
+            benchmark3_gpu!(src2, trg2, p, q)
             diff = abs.(trg .- trg2)
             err_norm = sqrt(sum(abs2, diff)/length(diff))
             diff_bool = diff .< eps(T)
             if all(diff_bool)
                 println("MATCHES")
             else
-                if nparticles < 10
-                    display(trg)
-                    display(trg2)
-                    display(diff)
+                if nparticles < 10 && debug
+                    display(trg[10:12])
+                    display(trg2[10:12])
+                    display(diff[10:12])
                 end
                 n_diff = count(==(false), diff_bool)
                 n_total = 3*size(trg, 2)
@@ -326,9 +333,6 @@ function main(run_option; nparticles=2^5, p=1, q=1, T=Float32)
     else
         # ns = 2 .^ collect(4:1:17)
         for np in nparticles
-            p = min(2^4, np, 1024)
-            q = 16
-
             println("No. of particles: $np")
             println("Tile size, p: $p")
             println("Cols per tile, q: $q")
@@ -345,5 +349,5 @@ function main(run_option; nparticles=2^5, p=1, q=1, T=Float32)
 end
 
 # Run_option - # [1]test [2]profile [3]benchmark
-main(1; nparticles=2^2, T=Float64)
-# main(3; nparticles=[2^5], T=Float32)
+# main(1; nparticles=2^2, p=1, T=Float64)
+main(3; nparticles=[2^5], T=Float32)
