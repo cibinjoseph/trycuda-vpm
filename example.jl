f847b019cda4b64cada987b6ea7614dcbd2d20ff
flowlab <rymanderson@gmail.com>
2024-06-03 15:34:52 -0600

    Try matrim mul examples

--- example.jl
+++ example.jl
@@ -101,6 +101,56 @@ function bench_vel1_mem!(v, y, x, nblocks_x, nblocks_y)
     end
 end
 
+function cpu_mul!(c, a, b)
+    for i in 1:size(a, 1)
+        for j in 1:size(b, 2)
+            for k in 1:size(a, 2)
+                c[i, j] += a[i, k] * b[k, j]
+            end
+        end
+    end
+    return nothing
+end
+
+function gpu_mul1!(c, a, b)
+    idx::Int32 = threadIdx().x + (blockIdx().x-1)*blockDim().x
+    idy::Int32 = threadIdx().y + (blockIdx().y-1)*blockDim().y
+    k::Int32 = 1
+    while k <= size(a, 2)
+        c[idx, idy] += a[idx, k] * b[k, idy]
+        k += 1
+    end
+
+    return nothing
+end
+
+function gpu_mul2!(c, a, b)
+    idx::Int32 = threadIdx().x + (blockIdx().x-1)*blockDim().x
+    idy::Int32 = threadIdx().y + (blockIdx().y-1)*blockDim().y
+    k::Int32 = 1
+    while k <= size(a, 2)
+        c[idx, idy] += a[idx, k] * b[k, idy]
+        k += 1
+    end
+
+    return nothing
+end
+
+function bench_gpu_mul1!(c, a, b)
+    c_d = CuArray(c)
+    a_d = CuArray(a)
+    b_d = CuArray(b)
+    m = size(a, 1)
+    n = size(b, 2)
+    bx = ceil(Int32, m/32)
+    by = ceil(Int32, n/32)
+    CUDA.@sync begin
+        @cuda threads=(32, 32) blocks=(bx, by) gpu_mul1!(c_d, a_d, b_d)
+    end
+    c = Array(c_d)
+    return nothing
+end
+
 # check array ops inside kernel
 function cpu_array_kernel!(v, y, x)
     v .= x.*v
@@ -119,9 +169,9 @@ function gpu_array_kernel!(v, y, x)
     return nothing
 end
 
-n = 2^12
-nf = 43
-T = Float32
+# n = 2^12
+# nf = 43
+# T = Float32
 
 # x = rand(T, nf, n)
 # y = rand(T, nf, n)
@@ -133,26 +183,39 @@ T = Float32
 # nblocks_y = ceil(Int, n/32)
 # @btime bench_addmat!(y_d, x_d, nblocks_x, nblocks_y)
 
-x = rand(T, nf, n)
-y = rand(T, nf, 1)
-v = rand(T, nf, n)
+# x = rand(T, nf, n)
+# y = rand(T, nf, 1)
+# v = rand(T, nf, n)
 # @btime cpu_vel1!(v, y, x)
 
 # x_d = CuArray(x)
 # y_d = CuArray(y)
 # v_d = CuArray(v)
-nblocks_x = 1
-nblocks_y = ceil(Int, n/32)
+# nblocks_x = 1
+# nblocks_y = ceil(Int, n/32)
 # @btime bench_vel1!(v_d, y_d, x_d, nblocks_x, nblocks_y)
 # @btime bench_vel1_mem!(v, y, x, nblocks_x, nblocks_y)
 
-x_d = CuArray(x)
-y_d = CuArray(y)
-v_d = CuArray(v)
-@btime cpu_array_kernel!($v, $y, $x)
+# x_d = CuArray(x)
+# y_d = CuArray(y)
+# v_d = CuArray(v)
+# @btime cpu_array_kernel!($v, $y, $x)
 
 # kernel = @cuda launch=false gpu_array_kernel!(v_d, y_d, x_d)
 # CUDA.launch_configuration(kernel.fun)
 
-@btime CUDA.@sync gpu_array_kernel!($v_d, $y_d, $x_d)
+# @btime CUDA.@sync gpu_array_kernel!($v_d, $y_d, $x_d)
 # CUDA.@profile trace=true gpu_array_kernel!(v_d, y_d, x_d)
+
+m = 2^12
+n = 2^12
+k = 2^12
+T = Float32
+
+a = rand(T, m, k)
+b = rand(T, k, n)
+c1 = zeros(T, m, n)
+c2 = zeros(T, m, n)
+
+@btime cpu_mul!(c1, a, b)
+@btime bench_gpu_mul1!(c2, a, b)
