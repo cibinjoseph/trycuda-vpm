2487efd46918c1fefc530deac542ee80a35ec16f
flowlab <rymanderson@gmail.com>
2024-06-05 16:33:16 -0600

    Run some tests with different ns and nt values

--- vpm.jl
+++ vpm.jl
@@ -291,6 +291,9 @@ function benchmark3_gpu!(s, t, p, q)
     view(t, 16:24, :) .= Array(t_d[16:24, :])
 end
 
+function get_launch_config(ns, nt, p, q; T=Float32)
+end
+
 function check_launch(n, p, q; T=Float32)
     max_threads_per_block = T==Float32 ? 1024 : 256
 
@@ -355,7 +358,7 @@ end
 # for i in 7:17
 #     main(3; n=2^i, p=256, T=Float32)
 # end
-main(1; ns=2, p=256, T=Float32, debug=true)
-# main(1; ns=1, nt=6, p=3, T=Float32, debug=true)
+# main(1; ns=2, p=256, T=Float32, debug=true)
+main(1; ns=8739, nt=3884, p=4, T=Float32, debug=true)
 # main(1; n=33, p=11, T=Float32)
 # main(1; n=130, p=26, q=2, T=Float64)
