220f9283e3e9e9fb92e397a401e903e1712a2c66
flowlab <rymanderson@gmail.com>
2024-08-27 16:10:00 -0600

    Test multiple streams with same kernel

--- trials/streams.jl
+++ trials/streams.jl
@@ -1,11 +1,24 @@
 using CUDA
 
-function kernel(x, n)
+function kernel(x, i)
     tid = threadIdx().x + blockDim().x * (blockIdx().x-1)
-    i = tid
-    while i <= n
-        x[i] = sqrt(3.14159^i)
-        i += blockDim().x * gridDim().x
+    j = 1
+    while j <= 10^4
+        x[tid, i] = sqrt(3.14159^3)
+        j += 1
+    end
+    return
+end
+
+function main(a, nstreams)
+    a_d = CuArray{eltype(a)}(undef, length(a), nstreams)
+
+    for i=1:nstreams
+        stream = CuStream()
+        @cuda threads=256 blocks=1 stream=stream kernel(a_d, i)
+
+        # Single stream
+        # @cuda threads=256 blocks=1 kernel(a_d, i)
     end
     return
 end
@@ -13,9 +26,6 @@ end
 n = 2^20
 nstreams = 8
 a = ones(n)
-a_d = CuArray{eltype(a)}(undef, n, nstreams)
 
-for i=1:nstreams
-    stream = CuStream()
-    @cuda threads=64 blocks=1 stream=stream kernel(a_d, n)
-end
+main(a, nstreams)
+CUDA.@profile main(a, nstreams)
