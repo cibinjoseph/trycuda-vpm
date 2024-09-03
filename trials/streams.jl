a90a0bc0c6eadba394d11b9d5e4c4ea252716b85
flowlab <rymanderson@gmail.com>
2024-09-03 16:12:31 -0600

    Test multiple streams

--- trials/streams.jl
+++ trials/streams.jl
@@ -1,31 +1,50 @@
 using CUDA
 
-function kernel(x, i)
+function kernel(x)
     tid = threadIdx().x + blockDim().x * (blockIdx().x-1)
     j = 1
     while j <= 10^4
-        x[tid, i] = sqrt(3.14159^3)
+        x[tid] = sqrt(3.14159^3)
         j += 1
     end
     return
 end
 
-function main(a, nstreams)
-    a_d = CuArray{eltype(a)}(undef, length(a), nstreams)
+function main(n)
+    a = ones(n)
+    b = ones(n)
+    c = ones(n)
+    d = ones(n)
 
-    for i=1:nstreams
-        stream = CuStream()
-        @cuda threads=256 blocks=1 stream=stream kernel(a_d, i)
+    @sync begin
+        @async begin
+            a_d = CuArray(a)
+            @cuda threads=256 blocks=2 kernel(a_d)
+            a .= Array(a_d)
+        end
 
-        # Single stream
-        # @cuda threads=256 blocks=1 kernel(a_d, i)
+        @async begin
+            b_d = CuArray(b)
+            @cuda threads=256 blocks=2 kernel(b_d)
+            b .= Array(b_d)
+        end
+
+        # @async begin
+        #     c_d = CuArray(c)
+        #     @cuda threads=256 blocks=2 kernel(c_d)
+        #     c .= Array(c_d)
+        # end
+        #
+        # @async begin
+        #     d_d = CuArray(d)
+        #     @cuda threads=256 blocks=2 kernel(d_d)
+        #     d .= Array(d_d)
+        # end
     end
     return
 end
 
-n = 2^20
-nstreams = 8
-a = ones(n)
+n = 2^18
 
-main(a, nstreams)
-CUDA.@profile main(a, nstreams)
+main(n)
+CUDA.@profile main(n)
