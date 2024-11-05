c0beda4ceebe54571d0e3395c24840de997298cc
Cibin Joseph <cibin@login04.rc.byu.edu>
2024-11-05 10:52:50 -0700

    Check speedup for kernel functions

--- trials/precision-perf.jl
+++ trials/precision-perf.jl
@@ -2,22 +2,43 @@ using CUDA
 using BenchmarkTools
 
 
-function benchs(xs)
-    CUDA.@sync ys = sin.(xs) .+ xs.^2 .+ cos.(sqrt.(xs)) .+ atan.(xs)
+function benchs(xs, ys)
+    CUDA.@sync ys .= sin.(xs) .+ xs.^2 .+ cos.(sqrt.(xs)) .+ atan.(xs)
     return
 end
 
-function benchd(xd)
-    CUDA.@sync yd = sin.(xd) .+ xd.^2 .+ cos.(sqrt.(xd)) .+ atan.(xd)
+function benchd(xd, yd)
+    CUDA.@sync yd .= sin.(xd) .+ xd.^2 .+ cos.(sqrt.(xd)) .+ atan.(xd)
+    return
+end
+
+function kernel(x)
+    i::Int32 = threadIdx().x + (blockIdx().x-1) * blockDim().x
+    xi = x[i]
+    xi = sin(xi) + xi^2 + cos(sqrt(xi)) + atan(xi)
+    i == 1 && @cushow typeof(xi)
+    return
+end
+
+function bench(x)
+    blocks = cld(length(x), 1024)
+    CUDA.@sync begin
+        @cuda threads=1024 blocks=blocks kernel(x)
+    end
     return
 end
 
 function run_benchmark(n)
     xs = CUDA.rand(n)
     xd = CUDA.rand(Float64, n)
+    ys = similar(xs)
+    yd = similar(xd)
 
-    ts = @benchmark benchs($xs)
-    td = @benchmark benchd($xd)
+    # ts = @benchmark benchs($xs, $ys)
+    # td = @benchmark benchd($xd, $yd)
+
+    ts = @benchmark bench($xs)
+    td = @benchmark bench($xd)
 
     # Time in microseconds
     tts = median(ts.times)/1000
@@ -26,7 +47,10 @@ function run_benchmark(n)
     return
 end
 
-for i = 5:24
-    n = 2^i
-    run_benchmark(n)
-end
+# for i = 5:35
+#     n = 2^i
+#     run_benchmark(n)
+# end
+
+n = 2^20
+run_benchmark(n)
