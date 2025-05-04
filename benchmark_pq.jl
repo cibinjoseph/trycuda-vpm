0dba89575b96f2410f982e2e1c1c74093b02f18f
Cibin Supercomputer <cibin@byu.edu>
2025-05-04 13:38:42 -0600

    Add provision to take no. of particles as input argument

--- benchmark_pq.jl
+++ benchmark_pq.jl
@@ -1,17 +1,20 @@
 include("vpm.jl")
 using Primes
 
-ns = 5000
-divs = sort(divisors(ns))
+# Read input arg if available
+n_default = 5000
+n = length(ARGS) == 0 ? n_default : parse(Int, ARGS[1])
+
+divs = sort(divisors(n))
 
 
 for p in divs
     divs_p = divisors(p)
     for q in divs
         if q in divs_p && p*q <= 512
-            main(3; ns=ns, p=p, q=q, show_pq=true, debug=false, algorithm=7, padding=false)
-        else
-            println("$ns $p $q nan")
+            main(3; ns=n, p=p, q=q, show_pq=true, debug=false, algorithm=7, padding=false)
+        # else
+        #     println("$n $p $q nan")
         end
     end
 end
--- benchmark_pq32.jl
+++ benchmark_pq32.jl
@@ -3,6 +3,11 @@ using StaticArrays
 include("vpm.jl")
 include("divs_512.jl")
 
+# Read input arg if available
+n_default = 5000
+n = length(ARGS) == 0 ? n_default : parse(Int, ARGS[1])
+
+# Assemble divisor pairs that result in 32 as product multiple
 pqs = []
 for i = 1:size(divs_512, 2)
     p = divs_512[1, i]
@@ -10,7 +15,6 @@ for i = 1:size(divs_512, 2)
     push!(pqs, (p, q))
 end
 
-const n = 17100
 for (p, q) in pqs
     _, _, _, _, speedup = main(3; nt=n, ns=n, p=p, q=q, debug=false, algorithm=7, padding=false, show_pq=true, return_vals=true)
 end
