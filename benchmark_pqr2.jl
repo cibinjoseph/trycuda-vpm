b34f97aff7d063142293d8d02fb3c11820f2e8a6
Cibin Supercomputer <cibin@byu.edu>
2025-08-09 19:22:11 -0600

    Add options for changing rectangular from arguments

--- benchmark_pqr2.jl
+++ benchmark_pqr2.jl
@@ -1,20 +1,29 @@
 include("vpm.jl")
 include("optimalpq.jl")
 
-rectangular = false
 
 # Read input arg if available
 nt_default = 512
+option_default = 0
 nt = length(ARGS) == 0 ? nt_default : parse(Int, ARGS[1])
+option = length(ARGS) <= 1 ? option_default : parse(Int, ARGS[2])
+
+# Rectangular if option >= 0
+rectangular = option >= 0 ? true : false
+
+multiple32 = true
+if option == 1
+    multiple32 = false
+end
 
 ar = collect(10:10:2000)
 
 for i in ar
     ns = Int32(nt * i)
     if rectangular
-        p, q, r = optimal_pqr(ns; multiple32=true)
+        p, q, r = optimal_pqr(ns; multiple32=multiple32, pmax=1)
     else
-        p, q, _ = optimal_pq(nt; multiple32=true)
+        p, q, _ = optimal_pq(nt; multiple32=multiple32)
         r = p
     end
 
